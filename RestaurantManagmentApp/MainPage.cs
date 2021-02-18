b8da8c803ad52d1bc674e7acf95f35b6f7c9282a
DoProjectProvider <doproject2068@gmail.com>
2021-02-19 01:36:50 +0700

    Normalize Code in Main Page

--- RestaurantManagmentApp/MainPage.cs
+++ RestaurantManagmentApp/MainPage.cs
@@ -23,6 +23,62 @@ namespace RestaurantManagmentApp
 
         private void MainPage_Load(object sender, EventArgs e)
         {
+            /*DataTable table = new DataTable();
+            *//*try
+            {*//*
+            con.Open();
+            string tableQuery = "SELECT * FROM `tablelist`";
+            MySqlDataReader tableData;
+            tableData = con.ExecuteReader(tableQuery);
+            if (tableData.HasRows)
+            {
+
+                Button buttonDynamic;
+                int x = 1;
+                int y = 1;
+
+                while (tableData.Read())
+                {
+
+                    //id = row["id"].ToString();
+                    tableNumber = tableData["tableNumber"].ToString();
+
+                    buttonDynamic = new Button();
+                    buttonDynamic.Text = tableNumber;
+                    buttonDynamic.Name = tableNumber;
+                    buttonDynamic.Size = new Size(100, 40);
+                    buttonDynamic.Location = new Point(102 *(x + 1), 45 * y);
+                    buttonDynamic.Click += (s, ee) =>
+                    {
+                        Button clickedButton = (Button)s;
+                        Form2 f2 = new Form2();
+                        f2.Show();
+                        f2.setLabel(clickedButton.Text);
+                    };
+
+
+                    this.tableList.Controls.Add(buttonDynamic);
+
+                    if (x == 6)
+                    {
+                        x = 0;
+                        y++;
+                    }
+
+                    x++;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Table Data not found", "Information");
+            }
+            con.Close();*/
+            loadTableData();
+            loadFoodData();
+        }
+
+        private void loadTableData()
+        {
             DataTable table = new DataTable();
             /*try
             {*/
@@ -47,7 +103,7 @@ namespace RestaurantManagmentApp
                     buttonDynamic.Text = tableNumber;
                     buttonDynamic.Name = tableNumber;
                     buttonDynamic.Size = new Size(100, 40);
-                    buttonDynamic.Location = new Point(25 *(x + 1), 45 * y);
+                    buttonDynamic.Location = new Point(102 * (x + 1), 45 * y);
                     buttonDynamic.Click += (s, ee) =>
                     {
                         Button clickedButton = (Button)s;
@@ -73,10 +129,7 @@ namespace RestaurantManagmentApp
                 MessageBox.Show("Table Data not found", "Information");
             }
             con.Close();
-
-            loadFoodData();
         }
-
         private void loadFoodData()
         {
 
